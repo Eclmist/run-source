c474ec92ee55a058643da342765dcf0e210f1879
Samuel Ng <eclmist@yahoo.com>
2018-08-26 18:50:42 +0800

    Initial Commit

---
+++ run-source.js
@@ -0,0 +1,27 @@
+const js_slang = require('js-slang');
+const fs = require('fs');
+const config = require('config');
+
+exports.runSource = runSource;
+function runSource(path) {
+    let code = '';
+    try {
+        code = fs.readFileSync(path, 'utf-8');
+    } catch (e) {
+        console.error('Error: Cannot find entry point \'' + path + '\'!');
+        return;
+    }
+    const context = js_slang.createContext();
+    context.chapter = config.get('source-chapter');
+
+    js_slang.runInContext(code, context, null).then(res => {
+        if (res.status === 'error') {
+            console.log(js_slang.parseError(context.errors));
+            return;
+        }
+        console.log(res.status);
+        console.log(res.value);
+    });
+}
+
+runSource(process.argv[2] || config.get('main'));
