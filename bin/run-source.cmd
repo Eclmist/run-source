5552e8de85eac9f70470489b8384be308bde8e9e
Samuel Ng <eclmist@yahoo.com>
2018-08-26 19:49:46 +0800

    Updated Binaries

--- bin/run-source.cmd
+++
@@ -1,8 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\run-source\bin\run-source" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\run-source\bin\run-source" %*
-  PAUSE
-)
